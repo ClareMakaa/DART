3921ce3b5ee3874a9c9df86739c7968e5748c46f
Clare Makaa <claremakaa1@gmail.com>
2024-03-05 09:42:40 +0300

    Variables

---
+++ variables.dart
@@ -0,0 +1,14 @@
+void main(){
+  //variables in Dart Language
+  String name = "Clare";
+  int age = 23;
+  String nationality = "Kenyan";
+  bool ismarried = false;
+
+  print (name);
+  print(age);
+  print(nationality);
+  print (ismarried);
+  
+  print("Hello my $name and i'm $age years old. I am a $nationality and my marriage status is $ismarried");
+}
