dc03f19c74570c69ecd46ba612d923af133e3981
Clare Makaa <claremakaa1@gmail.com>
2024-03-20 20:26:32 +0300

    DART

---
+++ HACKATHON 1/variables.dart
@@ -0,0 +1,10 @@
+void main() {
+  // Personal information
+  String name = "Clare Makaa";
+  int age = 25;
+  String school = "JKUAT University";
+  String hobby = "Playing guitar";
+
+  // Displaying personal information
+  print("My name is $name. I am $age years old, studying at $school, and my hobby is $hobby.");
+}
